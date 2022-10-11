1354920eb6cda25e29bea709d914c686c35390a7
VENUGOPALAN Uma (DIGIT-EXT) <Uma.VENUGOPALAN@ext.ec.europa.eu>
2022-10-11 08:30:46 +0200

    updated localstorage in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,61 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-
-import { EuiIconModule } from '@eui/components/atoms/eui-icon';
-import { EuiButtonModule } from '@eui/components/eui-button';
-import { EuiCardModule } from '@eui/components/eui-card';
-import { EuiLabelModule } from '@eui/components/atoms/eui-label';
-import { CommonModule } from '@angular/common';
 import { CoreModule } from './core/core.module';
-import { EuiAppModule, EuiNotificationsModule } from '@eui/components/layout';
+import { CoreModule as UxCoreModule,
+ StorageService, 
+ SessionStorageService, 
+ LocalStorageService,
+ AsyncStorageService,
+ LocalForageService, LOCAL_FORAGE_SERVICE_CONFIG_TOKEN} from '@eui/core';
+ import * as localForage from 'localforage';
+ 
+
+ import { FormsModule } from '@angular/forms';
+ 
+ import { HelloComponent } from './hello.component';
+ 
+ import { EuiIconModule } from '@eui/components/atoms/eui-icon';
+ import { EuiButtonModule } from '@eui/components/eui-button';
+ import { EuiCardModule } from '@eui/components/eui-card';
+ import { EuiLabelModule } from '@eui/components/atoms/eui-label';
+ import { CommonModule } from '@angular/common';
+ import { EuiAppModule, EuiNotificationsModule } from '@eui/components/layout';
+ import { CachePreventionInterceptor } from '@eui/core';
+ import { CorsSecurityInterceptor } from '@eui/core';
+ import { AddLangParamInterceptor } from '@eui/core';
+ import { HTTP_INTERCEPTORS } from '@angular/common/http';
+ 
+ 
 
 @NgModule({
-  imports: [
-    CoreModule,
-    CommonModule,
-    BrowserModule,
-    FormsModule,
-    EuiButtonModule,
-    EuiIconModule,
-    EuiCardModule,
-    EuiLabelModule,
-    EuiAppModule,
-    EuiNotificationsModule,
-  ],
-  declarations: [AppComponent, HelloComponent],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        CoreModule,
+        BrowserModule,
+        FormsModule,
+        EuiButtonModule,
+        EuiIconModule,
+        EuiCardModule,
+        EuiLabelModule,
+        EuiAppModule,
+        EuiNotificationsModule,
+      
+       
+    ],
+    providers: [
+        { provide: 'LocalStorageService', useExisting: LocalStorageService },
+        { provide: StorageService, useExisting: SessionStorageService },
+        
+    ],
+    bootstrap: [
+        AppComponent,
+    ],
 })
 export class AppModule {}
